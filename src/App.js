2ba4fe2c083d9e5f2af8d90fd00c30720d2ea753
Abhiman panwar <abhimanpanwar6@gmail.com>
2024-09-05 23:54:28 +0530

    update

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import PlagiarismChecker from './PlagiarismChecker';
+
+function App() {
+  return (
+    <div className="App">
+      <PlagiarismChecker />
+    </div>
+  );
+}
+
+export default App;
